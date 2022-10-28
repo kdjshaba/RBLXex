8eb8af9a9ecc6502f62ef3b2ad346cdf7e75baf7
Ossyence <ossyence@gmail.com>
2022-10-28 21:07:49 +0200

    RBLXex-V0.0.1-source
    Version 0.0.1 of RBLXex

---
+++ Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace RBLXex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.top_bar = new System.Windows.Forms.Panel();
+            this.seperator = new System.Windows.Forms.Panel();
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.minimize_button = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.open_file_button = new System.Windows.Forms.Button();
+            this.save_file_button = new System.Windows.Forms.Button();
+            this.close_button = new System.Windows.Forms.Button();
+            this.execute_button = new System.Windows.Forms.Button();
+            this.editor = new System.Windows.Forms.WebBrowser();
+            this.attach_button = new System.Windows.Forms.Button();
+            this.clear_button = new System.Windows.Forms.Button();
+            this.options_button = new System.Windows.Forms.Button();
+            this.top_bar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // top_bar
+            // 
+            this.top_bar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.top_bar.Controls.Add(this.seperator);
+            this.top_bar.Controls.Add(this.logo);
+            this.top_bar.Controls.Add(this.minimize_button);
+            this.top_bar.Controls.Add(this.title);
+            this.top_bar.Controls.Add(this.open_file_button);
+            this.top_bar.Controls.Add(this.save_file_button);
+            this.top_bar.Controls.Add(this.close_button);
+            this.top_bar.ForeColor = System.Drawing.Color.White;
+            this.top_bar.Location = new System.Drawing.Point(0, 0);
+            this.top_bar.Name = "top_bar";
+            this.top_bar.Size = new System.Drawing.Size(650, 34);
+            this.top_bar.TabIndex = 0;
+            this.top_bar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.top_bar_MouseDown);
+            this.top_bar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.top_bar_MouseMove);
+            // 
+            // seperator
+            // 
+            this.seperator.BackColor = System.Drawing.Color.White;
+            this.seperator.Location = new System.Drawing.Point(233, 8);
+            this.seperator.Name = "seperator";
+            this.seperator.Size = new System.Drawing.Size(1, 20);
+            this.seperator.TabIndex = 9;
+            // 
+            // logo
+            // 
+            this.logo.BackColor = System.Drawing.Color.Transparent;
+            this.logo.Image = ((System.Drawing.Image)(resources.GetObject("logo.Image")));
+            this.logo.Location = new System.Drawing.Point(3, 0);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(34, 34);
+            this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.logo.TabIndex = 8;
+            this.logo.TabStop = false;
+            // 
+            // minimize_button
+            // 
+            this.minimize_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.minimize_button.FlatAppearance.BorderSize = 0;
+            this.minimize_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimize_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minimize_button.ForeColor = System.Drawing.Color.White;
+            this.minimize_button.Location = new System.Drawing.Point(556, 0);
+            this.minimize_button.Name = "minimize_button";
+            this.minimize_button.Size = new System.Drawing.Size(47, 34);
+            this.minimize_button.TabIndex = 7;
+            this.minimize_button.Text = "-";
+            this.minimize_button.UseVisualStyleBackColor = false;
+            this.minimize_button.Click += new System.EventHandler(this.minimize_button_Click);
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.Transparent;
+            this.title.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold);
+            this.title.ForeColor = System.Drawing.Color.White;
+            this.title.Location = new System.Drawing.Point(40, 4);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(79, 25);
+            this.title.TabIndex = 6;
+            this.title.Text = "RBLXex";
+            // 
+            // open_file_button
+            // 
+            this.open_file_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.open_file_button.FlatAppearance.BorderSize = 0;
+            this.open_file_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.open_file_button.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.open_file_button.ForeColor = System.Drawing.Color.White;
+            this.open_file_button.Location = new System.Drawing.Point(240, 5);
+            this.open_file_button.Name = "open_file_button";
+            this.open_file_button.Size = new System.Drawing.Size(102, 25);
+            this.open_file_button.TabIndex = 3;
+            this.open_file_button.Text = "Open File";
+            this.open_file_button.UseVisualStyleBackColor = false;
+            this.open_file_button.Click += new System.EventHandler(this.open_file_button_Click);
+            // 
+            // save_file_button
+            // 
+            this.save_file_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.save_file_button.FlatAppearance.BorderSize = 0;
+            this.save_file_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.save_file_button.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.save_file_button.ForeColor = System.Drawing.Color.White;
+            this.save_file_button.Location = new System.Drawing.Point(124, 5);
+            this.save_file_button.Name = "save_file_button";
+            this.save_file_button.Size = new System.Drawing.Size(103, 25);
+            this.save_file_button.TabIndex = 4;
+            this.save_file_button.Text = "Save File";
+            this.save_file_button.UseVisualStyleBackColor = false;
+            this.save_file_button.Click += new System.EventHandler(this.save_file_button_Click);
+            // 
+            // close_button
+            // 
+            this.close_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.close_button.FlatAppearance.BorderSize = 0;
+            this.close_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.close_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close_button.ForeColor = System.Drawing.Color.White;
+            this.close_button.Location = new System.Drawing.Point(603, 0);
+            this.close_button.Name = "close_button";
+            this.close_button.Size = new System.Drawing.Size(47, 34);
+            this.close_button.TabIndex = 5;
+            this.close_button.Text = "X";
+            this.close_button.UseVisualStyleBackColor = false;
+            this.close_button.Click += new System.EventHandler(this.close_button_Click);
+            // 
+            // execute_button
+            // 
+            this.execute_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(59)))), ((int)(((byte)(115)))));
+            this.execute_button.FlatAppearance.BorderSize = 0;
+            this.execute_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.execute_button.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.execute_button.ForeColor = System.Drawing.Color.White;
+            this.execute_button.Location = new System.Drawing.Point(0, 332);
+            this.execute_button.Name = "execute_button";
+            this.execute_button.Size = new System.Drawing.Size(163, 34);
+            this.execute_button.TabIndex = 2;
+            this.execute_button.Text = "Execute";
+            this.execute_button.UseVisualStyleBackColor = false;
+            this.execute_button.Click += new System.EventHandler(this.execute_button_Click);
+            // 
+            // editor
+            // 
+            this.editor.Location = new System.Drawing.Point(0, 34);
+            this.editor.MinimumSize = new System.Drawing.Size(20, 20);
+            this.editor.Name = "editor";
+            this.editor.Size = new System.Drawing.Size(650, 298);
+            this.editor.TabIndex = 10;
+            // 
+            // attach_button
+            // 
+            this.attach_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(38)))), ((int)(((byte)(77)))));
+            this.attach_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.attach_button.FlatAppearance.BorderSize = 0;
+            this.attach_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.attach_button.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.attach_button.ForeColor = System.Drawing.Color.White;
+            this.attach_button.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.attach_button.Location = new System.Drawing.Point(163, 332);
+            this.attach_button.Name = "attach_button";
+            this.attach_button.Size = new System.Drawing.Size(162, 34);
+            this.attach_button.TabIndex = 5;
+            this.attach_button.Text = "Attach";
+            this.attach_button.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.attach_button.UseVisualStyleBackColor = false;
+            this.attach_button.Click += new System.EventHandler(this.attach_button_Click);
+            // 
+            // clear_button
+            // 
+            this.clear_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.clear_button.FlatAppearance.BorderSize = 0;
+            this.clear_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clear_button.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear_button.ForeColor = System.Drawing.Color.White;
+            this.clear_button.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.clear_button.Location = new System.Drawing.Point(325, 332);
+            this.clear_button.Name = "clear_button";
+            this.clear_button.Size = new System.Drawing.Size(163, 34);
+            this.clear_button.TabIndex = 3;
+            this.clear_button.Text = "Clear";
+            this.clear_button.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.clear_button.UseVisualStyleBackColor = false;
+            this.clear_button.Click += new System.EventHandler(this.clear_button_Click);
+            // 
+            // options_button
+            // 
+            this.options_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.options_button.FlatAppearance.BorderSize = 0;
+            this.options_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.options_button.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.options_button.ForeColor = System.Drawing.Color.White;
+            this.options_button.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.options_button.Location = new System.Drawing.Point(488, 332);
+            this.options_button.Name = "options_button";
+            this.options_button.Size = new System.Drawing.Size(162, 34);
+            this.options_button.TabIndex = 11;
+            this.options_button.Text = "Options";
+            this.options_button.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.options_button.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(650, 366);
+            this.Controls.Add(this.options_button);
+            this.Controls.Add(this.editor);
+            this.Controls.Add(this.attach_button);
+            this.Controls.Add(this.clear_button);
+            this.Controls.Add(this.execute_button);
+            this.Controls.Add(this.top_bar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "RBLXex";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.top_bar.ResumeLayout(false);
+            this.top_bar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel top_bar;
+        private System.Windows.Forms.Button execute_button;
+        private System.Windows.Forms.Button clear_button;
+        private System.Windows.Forms.Button open_file_button;
+        private System.Windows.Forms.Button save_file_button;
+        private System.Windows.Forms.Button close_button;
+        private System.Windows.Forms.Button minimize_button;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button attach_button;
+        private System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.Panel seperator;
+        private System.Windows.Forms.WebBrowser editor;
+        private System.Windows.Forms.Button options_button;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,298 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeAreDevs_API;
+
+namespace RBLXex
+{
+    public partial class Form1 : Form
+    {
+        private ExploitAPI api = new ExploitAPI();
+        private WebClient webClient = new WebClient();
+
+        private Point lastPoint;
+
+        private bool busyAttaching = false;
+
+        private static readonly string robloxName = "RobloxPlayerBeta";
+        private static readonly string monacoPath = Path.Combine(Application.StartupPath, @"Monaco");
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Custom Functions
+
+        private void SetupIntellisense()
+        {
+            void AddIntellisense(string label, string kind, string detail, string insertText)
+            {
+                string text = "\"" + label + "\"";
+                string text2 = "\"" + kind + "\"";
+                string text3 = "\"" + detail + "\"";
+                string text4 = "\"" + insertText + "\"";
+
+                editor.Document.InvokeScript("AddIntellisense", new object[]
+                {
+                    label,
+                    kind,
+                    detail,
+                    insertText
+                });
+            }
+
+            foreach (string text in File.ReadAllLines(monacoPath + "/globalf.txt"))
+            {
+                bool flag = text.Contains(':');
+
+                if (flag)
+                {
+                    AddIntellisense(text, "Function", text, text.Substring(1));
+                }
+                else
+                {
+                    AddIntellisense(text, "Function", text, text);
+                }
+            }
+
+            foreach (string text in File.ReadLines(monacoPath + "/globalv.txt"))
+            {
+                AddIntellisense(text, "Variable", text, text);
+            }
+
+            foreach (string text in File.ReadLines(monacoPath + "/globalns.txt"))
+            {
+                AddIntellisense(text, "Class", text, text);
+            }
+
+            foreach (string text in File.ReadLines(monacoPath + "/classfunc.txt"))
+            {
+                AddIntellisense(text, "Method", text, text);
+            }
+
+            foreach (string text in File.ReadLines(monacoPath + "/base.txt"))
+            {
+                AddIntellisense(text, "Keyword", text, text);
+            }
+        }
+
+        private string GetEditorValue() {
+            object[] args = new string[0];
+            object obj = editor.Document.InvokeScript("GetText", args);
+            string script = obj.ToString();
+
+            return script;
+        }
+
+        private void SetEditorValue(string value)
+        {
+            editor.Document.InvokeScript("SetText", new object[] { value });
+        }
+
+        private void ShowMessage(string msg) { 
+            Task.Run(() => { MessageBox.Show(msg); }); 
+        }
+
+        private bool IsAppRunning(string appName)
+        {
+            //Checks if an app is running
+
+            if (Process.GetProcesses().Any((p) => p.ProcessName.Contains(appName))) { return true; }
+
+            return false;
+        }
+
+        private void PrepareForAttachment()
+        {
+            //Bypass API launch error by deleting a finj.exe (kinda hacky ik but it has to be done)
+            //Error was "You are using an outdated version of the WeAreDevs_API"
+
+            //Removing finj.exe fixes it somehow (I didnt read the documentation so I dont understand this)
+            
+            try
+            {
+                //File.Delete(@".\WRDAPICONF.json");
+                //File.Delete(@".\exploit-main.dll");
+                //File.Delete(@".\FastColorTextBox.xml");
+                //File.Delete(@".\kernel64.sys.dll");
+                //File.Delete(@".\Newtonsoft.Json.dll");
+                File.Delete(@".\finj.exe");
+            }
+            catch { }
+        }
+
+        private void CloseProcess(string name)
+        {
+            //Closes any process that has the name specified
+
+            Process[] processes = null;
+
+            try
+            {
+                processes = Process.GetProcessesByName(name);
+
+                Process mainProcess = processes[0];
+
+                if (!mainProcess.HasExited)
+                {
+                    mainProcess.Kill();
+                }
+            }
+            finally
+            {
+                if (processes != null)
+                {
+                    foreach (Process process in processes)
+                    {
+                        process.Dispose();
+                    }
+                }
+            }
+        }
+
+        private void Attach()
+        {
+            ShowMessage("Attachment process has begun, this may take a while to startup");
+
+            PrepareForAttachment();
+
+            busyAttaching = true;
+            api.LaunchExploit(); //Begin attaching
+
+            while (!api.isAPIAttached()) { Thread.Sleep(50); }
+
+            busyAttaching = false;
+            CloseProcess("finj"); //Close the annoying command prompt after attachment
+
+            ShowMessage("Attachment has finished");
+        }
+
+        //UI Events
+        private void minimize_button_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void close_button_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void attach_button_Click(object sender, EventArgs e)
+        {
+            if (busyAttaching) { ShowMessage("Busy attaching, please wait."); return; }
+            if (!IsAppRunning(robloxName)) { ShowMessage("Cant attach without ROBLOX running."); return; }
+            if (api.isAPIAttached()) { ShowMessage("Already attached to ROBLOX client."); return; }
+
+            Task.Run(() => {
+                Attach();
+            });
+        }
+
+        private void execute_button_Click(object sender, EventArgs e)
+        {
+            void SendEditorValueToApi()
+            {
+                //I have to use this because it requires a switch back to the UI thread to get editors value
+                api.SendLuaScript(GetEditorValue());
+            }
+
+            if (busyAttaching) { ShowMessage("Busy attaching, please wait."); return; }
+            if (!IsAppRunning(robloxName)) { ShowMessage("Cant attach or execute without ROBLOX running."); return; }
+
+            Task.Run(() => {
+                if (!api.isAPIAttached()) { Attach(); }
+
+                Invoke(new MethodInvoker(SendEditorValueToApi));
+            });
+        }
+
+        private void clear_button_Click(object sender, EventArgs e)
+        {
+            //Clear editor
+            SetEditorValue("");
+        }
+
+        private void open_file_button_Click(object sender, EventArgs e)
+        {
+            //Open a file to change the content of the text editor
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                dialog.Title = "Open";
+                SetEditorValue(File.ReadAllText(dialog.FileName));
+            }
+        }
+
+        private void save_file_button_Click(object sender, EventArgs e)
+        {
+            //Create a new file with contents of the text editor
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Lua File|*.lua|Text File|*.txt"; //Lets user decide which one to use (lua or txt)
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                using (Stream stream = File.Open(dialog.FileName, FileMode.CreateNew)) //Create new file with name chosen
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.Write(GetEditorValue()); //Set contents of file to text editors contents
+                }
+            }
+        }
+
+        private void top_bar_MouseDown(object sender, MouseEventArgs mouse)
+        {
+            //Mark point
+            lastPoint = new Point(mouse.X, mouse.Y);
+        }
+
+        private void top_bar_MouseMove(object sender, MouseEventArgs mouse)
+        {
+            //Move top bar around based on last point and mouses position
+            if (mouse.Button == MouseButtons.Left)
+            {
+                this.Left += mouse.X - lastPoint.X;
+                this.Top += mouse.Y - lastPoint.Y;
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            webClient.Proxy = null;
+
+            //Edit the registry to make sure the WebBrowser emulates a browser correctly so Monaco can run in it
+            //I tried using WebView2 but it didnt work properly so I have to use stupid WebBrowser that is inneficient and uses alot more memory
+            //also WebView2 was more difficult to get info out of so
+
+            try {
+                RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION", true);
+                
+                string friendlyName = AppDomain.CurrentDomain.FriendlyName;
+                bool flag2 = key.GetValue(friendlyName) == null;
+
+                if (flag2)
+                {
+                    key.SetValue(friendlyName, 11001, RegistryValueKind.DWord); //Edit the registry
+                }
+            } catch {}
+
+            editor.Url = new Uri(string.Format(monacoPath + "/Monaco.html", Directory.GetCurrentDirectory()));
+
+            await Task.Delay(500); //Wait 0.5 seconds so website can load up
+
+            editor.Document.InvokeScript("SetTheme", new string[] { "roblox-dark" });
+            SetEditorValue("--Developed by: Ossyence\nprint('Hello, world!')");
+            
+            SetupIntellisense();
+        }
+    }
+}
